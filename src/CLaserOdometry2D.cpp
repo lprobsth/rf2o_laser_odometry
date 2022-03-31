b69b128a7b35014aa67c37138cc290442068f62f
Lukas Probsthain <lukas.probsthain@gmail.com>
2022-03-31 17:23:16 +0000

    fix odometry orientation

--- src/CLaserOdometry2D.cpp
+++ src/CLaserOdometry2D.cpp
@@ -931,8 +931,8 @@ void CLaserOdometry2D::PoseUpdate()
   Pose3d pose_aux_2D = Pose3d::Identity();
 
   pose_aux_2D = rf2o::matrixYaw(double(kai_loc_(2)/fps));
-  pose_aux_2D.translation()(0) = acu_trans(0,2);
-  pose_aux_2D.translation()(1) = acu_trans(1,2);
+  pose_aux_2D.translation()(0) = -acu_trans(0,2);
+  pose_aux_2D.translation()(1) = -acu_trans(1,2);
 
 #ifdef DISABLE_VELOCITY_FILTER
   // Skip integration if scan matching failed and acu_trans, kai_loc_
@@ -972,7 +972,7 @@ void CLaserOdometry2D::PoseUpdate()
   //-------------------------------------------------------------------------------------
   double time_inc_sec = (current_scan_time - last_odom_time).toSec();
   last_odom_time = current_scan_time;
-  lin_speed = acu_trans(0,2) / time_inc_sec;
+  lin_speed = -acu_trans(0,2) / time_inc_sec;
   //double lin_speed = sqrt( mrpt::math::square(robot_oldpose.x()-robot_pose.x()) + mrpt::math::square(robot_oldpose.y()-robot_pose.y()) )/time_inc_sec;
 
   double ang_inc = rf2o::getYaw(robot_pose_.rotation()) -
